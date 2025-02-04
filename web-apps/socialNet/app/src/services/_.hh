d736841d525784d32f545783de2e54133987a0ca
Emile Cadorel <ecadorel@gmail.com>
2025-02-04 13:55:17 +0100

    fix

--- web-apps/socialNet/app/src/services/_.hh
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "compose/service.hh"
-#include "post/service.hh"
-#include "short_url/service.hh"
-#include "social_graph/service.hh"
-#include "text/service.hh"
-#include "timeline/service.hh"
-#include "user/service.hh"
